921c5feb6be225e91911546c61905f9d44b81dec
jiaQiu9 <351470636@qq.com>
2022-03-06 23:18:15 -0500

    done with project structure, P6

---
+++ P6/run.py
@@ -0,0 +1,5 @@
+from market import app
+# the main idea was to package everything related to the market
+# and have the application do everything for us
+if __name__=="__main__":
+    app.run(debug=True)
